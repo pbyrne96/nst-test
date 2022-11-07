8cbec8c72fe40c3c3ed141ba3faedb066dc30146
pbyrne96 <p.byrne@weareeight.com>
2022-11-07 17:51:46 +0000

    adding start of dataLake insertion

--- persamed/models/producers/OnDataLake/pdfToDataLake.ts
+++ persamed/models/producers/OnDataLake/pdfToDataLake.ts
@@ -1,8 +1,12 @@
 // uses the worker - producer architecture to write pdf's that have been scanned into a data lake or warehouse.
-
 interface DataLakeInsertion<T> {
   timeStamp: string;
   jobId: string;
   dataGiven: T;
 }
-export class dataLakeStage {}
+
+export class dataLakeStage<T> implements DataLakeInsertion<T> {
+  timeStamp: string;
+  jobId: string;
+  dataGiven: T;
+}
